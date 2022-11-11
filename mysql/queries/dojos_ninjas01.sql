94516bab782f83f366c08c2d22d74f4fc606f2f4
eneapoloska <enea.poloska@gmail.com>
2022-11-11 15:33:43 +0100

    dojoandninjas

--- mysql/queries/dojos_ninjas01.sql
+++ mysql/queries/dojos_ninjas01.sql
@@ -1,11 +1,41 @@
 use dojos_and_ninjas_schema;
 select * from dojos;
+insert into dojos (name) values ('dojo1');
+insert into dojos (name) values ('dojo2');
+insert into dojos (name) values ('dojo3');
+delete from dojos;
+select * from dojos;
+insert into dojos (name) values ('dojo4');
+insert into dojos (name) values ('dojo5');
+insert into dojos (name) values ('dojo6');
+select * from dojos;
 
-insert into dojos (name) values ('durres');
-
-update dojos set name = 'shkoder' where id = 3;
 select * from ninjas;
-insert into ninjas (first_name, last_name, age, dojo_id) values ('Endi', 'Mimini', 24, 1);
-insert into ninjas (first_name, last_name, age, dojo_id) values ('Endi2', 'Mimini', 24, 2);
+insert into ninjas (first_name, last_name, age, dojo_id) values ('ninja1', 'jujitsu', 228, 7);
+update ninjas set age = 28 where id = 4;
+insert into ninjas (first_name, last_name, age, dojo_id) values ('ninja2', 'jujitsu', 25, 7);
+insert into ninjas (first_name, last_name, age, dojo_id) values ('ninja3', 'jujitsu', 21, 7);
+select * from ninjas;
 
-select dojos.name as qyteti, ninjas.first_name as qytetari  from dojos left join ninjas on ninjas.dojo_id = dojos.id  where dojos.id=1;
+insert into ninjas (first_name, last_name, age, dojo_id) values ('ninja4', 'jing', 20, 8);
+insert into ninjas (first_name, last_name, age, dojo_id) values ('ninja5', 'jing', 23, 8);
+insert into ninjas (first_name, last_name, age, dojo_id) values ('ninja6', 'jing', 30, 8);
+select * from ninjas;
+
+insert into ninjas (first_name, last_name, age, dojo_id) values ('ninja7', 'sake', 30, 9);
+insert into ninjas (first_name, last_name, age, dojo_id) values ('ninja8', 'sake', 26, 9);
+insert into ninjas (first_name, last_name, age, dojo_id) values ('ninja9', 'sake', 22, 9);
+select * from ninjas;
+
+select * from ninjas where dojo_id = 7;
+select * from ninjas where dojo_id = 9;
+select * from ninjas where id =12;
+
+
+
+
+
+
+#SET SQL_SAFE_UPDATES = 0;
+#SET FOREIGN_KEY_CHECKS=0; -- to disable them
+#SET FOREIGN_KEY_CHECKS=1; -- to re-enable them
